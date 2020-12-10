3c208b0fea3ffef0db671d62ece957c38db18da2
derpicknicker1 <10minutemail@web.de>
2020-12-10 10:36:20 +0100

    Solution Day 10

--- src/advent.h
+++ src/advent.h
@@ -28,98 +28,98 @@ void get0b(char *f);
 void get1a(char *f);
 void get1b(char *f);
 
-//Day 2
+// Day 2
 void get2a(char *f);
 void get2b(char *f);
 
-//Day 3
+// Day 3
 void get3a(char *f);
 void get3b(char *f);
 
-//Day 4
+// Day 4
 void get4a(char *f);
 void get4b(char *f);
 
-//Day 5
+// Day 5
 void get5a(char *f);
 void get5b(char *f);
 
-//Day 6
+// Day 6
 void get6a(char *f);
 void get6b(char *f);
 
-//Day 7
+// Day 7
 void get7a(char *f);
 void get7b(char *f);
 
-//Day 8
+// Day 8
 void get8a(char *f);
 void get8b(char *f);
 
-//Day 9
+// Day 9
 void get9a(char *f);
 void get9b(char *f);
 
-//Day 10
+// Day 10
 void get10a(char *f);
 void get10b(char *f);
 
-//Day 10
+// Day 10
 void get11a(char *f);
 void get11b(char *f);
 
-//Day 12
+// Day 12
 void get12a(char *f);
 void get12b(char *f);
 
-//Day 13
+// Day 13
 void get13a(char *f);
 void get13b(char *f);
 
-//Day 14
+// Day 14
 void get14a(char *f);
 void get14b(char *f);
 
-//Day 15
+// Day 15
 void get15a(char *f);
 void get15b(char *f);
 
-//Day 16
+// Day 16
 void get16a(char *f);
 void get16b(char *f);
 
-//Day 17
+// Day 17
 void get17a(char *f);
 void get17b(char *f);
 
-//Day 18
+// Day 18
 void get18a(char *f);
 void get18b(char *f);
 
-//Day 19
+// Day 19
 void get19a(char *f);
 void get19b(char *f);
 
-//Day 20
+// Day 20
 void get20a(char *f);
 void get20b(char *f);
 
-//Day 21
+// Day 21
 void get21a(char *f);
 void get21b(char *f);
 
-//Day 22
+// Day 22
 void get22a(char *f);
 void get22b(char *f);
 
-//Day 23
+// Day 23
 void get23a(char *f);
 void get23b(char *f);
 
-//Day 24
+// Day 24
 void get24a(char *f);
 void get24b(char *f);
 
-//Day 25
+// Day 25
 void get25a(char *f);
 void get25b(char *f);
